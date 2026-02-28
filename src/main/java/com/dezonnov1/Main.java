46fa7dd01186705fe110cf24ced556bc486ee7ec
dezonnov1 <daniil73k@gmail.com>
2026-02-28 13:47:09 +0400

    Task 2 Выполнена подзадача 1 + Создайте массив, представляющий годы выпуска 50 случайных машин (от 2000 до 2025). + Выведите только машины, выпущенные после 2015 года. + Посчитайте средний возраст авто.

--- src/main/java/com/dezonnov1/Main.java
+++ src/main/java/com/dezonnov1/Main.java
@@ -1,7 +1,35 @@
 package com.dezonnov1;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+        //
+        task1();
+
 
     }
+
+
+    public static void task1(){
+        Random random = new Random();
+        // 50 случайных машин (от 2000 до 2025).
+        int[] years = random.ints(50,2000,2025+1)
+                .toArray();
+        System.out.println("Все 50 машин:\n"+Arrays.toString(years));
+
+        // Выведите только машины, выпущенные после 2015 года.
+        int[] yearsAfter2015 =
+                Arrays.stream(years)
+                .filter(value -> value > 2015)
+                .toArray();
+        System.out.println("Выпущенные после 2015 года:\n"+Arrays.toString(yearsAfter2015));
+
+        // Средний возраст авто
+        double avgAge = LocalDate.now().getYear() - (double)Arrays.stream(years).sum() / years.length;
+        System.out.println("Средний возраст авто:\n"+avgAge);
+    }
+
 }
