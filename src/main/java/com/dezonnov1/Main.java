c8e5a6d287a428f8adcf7e01df9876aaeaabde4c
dezonnov1 <daniil73k@gmail.com>
2026-02-28 16:23:56 +0400

    Task 2 Выполнена подзадача 2 + Создайте список с названиями моделей машин (например: Toyota Camry, BMW X5). + Удалите дубликаты, затем отсортируйте модели в обратном алфавитном порядке, выведите на экран, затем сохраните в Set. + Реализуйте проверку: если модель содержит слово "Tesla", замените её на "ELECTRO_CAR".

--- src/main/java/com/dezonnov1/Main.java
+++ src/main/java/com/dezonnov1/Main.java
@@ -1,14 +1,15 @@
 package com.dezonnov1;
 
 import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        //
+        System.out.println("===Task1===");
         task1();
-
+        System.out.println("===Task2===");
+        task2();
 
     }
 
@@ -31,5 +32,38 @@ public class Main {
         double avgAge = LocalDate.now().getYear() - (double)Arrays.stream(years).sum() / years.length;
         System.out.println("Средний возраст авто:\n"+avgAge);
     }
+    public static void task2(){
+        // Список с названиями моделей машин
+        String[] carMas = {
+                "Toyota Camry",
+                "Honda Civic",
+                "Hyundai Tucson",
+                "Ford Mustang",
+                "Ford Mustang",
+                "Tesla model X",
+                "Ford Mustang",
+                "BMW M5 E34",
+                "Mercedes-Benz E320 C-Class",
+                "Audi A4",
+                "Audi A4",
+                "Tesla Model S",
+                "Tesla Model S",
+                "Nissan Altima",
+                "Hyundai Tucson",
+                "Kia RIO",
+                "Hyundai Tucson",
+                "Kia RIO",
+                "Kia RIO"
+        };
 
+        // Удаление дубликатов и отсортировка моделей в обратном алфавитном порядке
+
+        Set<String> carSetTeslaLess = Arrays.stream(carMas)
+                .distinct()
+                .map(value -> value.contains("Tesla") ? "ELECTRO_CAR" : value)// Проверка на "Tesla" и замена "ELECTRO_CAR"
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+        // Вывод результата
+        System.out.println("Результат замены \"Tesla\" на \"ELECTRO_CAR\" :\n"+carSetTeslaLess);
+    }
 }
